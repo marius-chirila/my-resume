a742e42ee7e6f659399385aa35562ac5296a4132
Marius Chirila <marius.chirila.91@gmail.com>
2021-09-16 19:17:28 +0200

    Local hit counter in progress..

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,3 +1,9 @@
-const getVisitorCount = count => {
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVisitorCount();
+});
 
+const getVisitorCount = () => {
+    let count = 1;
+    document.getElementById('counting').innerText = count;
+    return count;
 }
