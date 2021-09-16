42b77506250d7e2b3089e99626565c67318ea34b
Marius Chirila <marius.chirila.91@gmail.com>
2021-09-16 23:31:25 +0200

    minor ads/fetching jscript

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,9 +1,15 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     visitCount();
 });
-
+const apiFetch= ''
 const visitCount = () => {
     let count;
-    document.getElementById('counting').innerText = count;
-    return count;
-}
+    fetch(apiFetch).then(response => {
+        return response.json();
+        }).then(response =>{
+            let count = response.count;
+            document.getElementById('counting').innerText = count;
+        }).catch(err => console.log('Not really fetched.' + err.message));
+        return count;
+    };
+   
